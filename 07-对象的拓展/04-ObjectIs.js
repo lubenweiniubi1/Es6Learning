ae199b2f3742ad9fd0377a13375c5f50848f44ed
潘林峰 <panlinfeng@hive-intel.com>
2020-09-14 09:39:31 +0800

    z

--- 07-对象的拓展/04-ObjectIs.js
+++ 07-对象的拓展/04-ObjectIs.js
@@ -7,6 +7,26 @@
 //Object.is就是部署这个算法的新方法。它用来比较两个值是否严格相等，与严格比较运算符（===）的行为基本一致。
 console.log(Object.is("foo", "foo")) //true
 console.log(Object.is({}, {})) //false
-console.log({} === {})//false
+console.log({} === {}) //false
 
 //不同之处只有两个：一是+0不等于-0，二是NaN等于自身。
+console.log(+0 === -0) //true
+console.log(NaN === NaN) //false
+
+console.log(Object.is(+0, -0)) //false
+console.log(Object.is(NaN, NaN)) //true
+
+//ES5可以通过下面的代码，部署Object.is。
+Object.defineProperty(Object, 'is', {
+    value: function(x, y) {
+      if (x === y) {
+        // 针对+0 不等于 -0的情况
+        return x !== 0 || 1 / x === 1 / y;
+      }
+      // 针对NaN的情况
+      return x !== x && y !== y;
+    },
+    configurable: true,
+    enumerable: false,
+    writable: true
+  });
