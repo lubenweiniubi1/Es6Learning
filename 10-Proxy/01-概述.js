cda05ca58abdf99a90e2f7c42093f810e071ac8d
潘林峰 <panlinfeng@hive-intel.com>
2020-10-09 09:21:41 +0800

    zzz

--- 10-Proxy/01-概述.js
+++ 10-Proxy/01-概述.js
@@ -71,3 +71,87 @@ var proxy = new Proxy(
 var obj = Object.create(proxy)
 console.log(obj.z) //36
 //上面代码中，proxy对象是obj对象的原型，obj对象本身并没有time属性，所以根据原型链，会在proxy对象上读取该属性，导致被拦截。
+
+//同一个拦截器函数，可以设置拦截多个操作。
+var handler = {
+  get: function (target, name) {
+    if (name === "prototype") {
+      return Object.prototype
+    }
+    return "Hello, " + name
+  },
+  apply: function (target, thisBinding, args) {
+    console.log("函数调用", args)
+    return args[0]
+  },
+  construct: function (target, args) {
+    console.log("aaa", args)
+    return { value: args[1] }
+  },
+}
+var fproxy = new Proxy(function (x, y) {
+  return x + y
+}, handler)
+console.log(fproxy(1, 2)) //函数调用 [ 1, 2 ]  1
+console.log(new fproxy()) //aaa []  { value: undefined }
+
+//下面是 Proxy 支持的拦截操作一览。对于可以设置、但没有设置拦截的操作，则直接落在目标对象上，按照原先的方式产生结果。
+/**
+ * 
+（1）get(target, propKey, receiver)
+
+拦截对象属性的读取，比如proxy.foo和proxy['foo']。
+
+最后一个参数receiver是一个对象，可选，参见下面Reflect.get的部分。
+
+（2）set(target, propKey, value, receiver)
+
+拦截对象属性的设置，比如proxy.foo = v或proxy['foo'] = v，返回一个布尔值。
+
+（3）has(target, propKey)
+
+拦截propKey in proxy的操作，返回一个布尔值。
+
+（4）deleteProperty(target, propKey)
+
+拦截delete proxy[propKey]的操作，返回一个布尔值。
+
+（5）ownKeys(target)
+
+拦截Object.getOwnPropertyNames(proxy)、Object.getOwnPropertySymbols(proxy)、Object.keys(proxy)，返回一个数组。该方法返回目标对象所有自身的属性的属性名，而Object.keys()的返回结果仅包括目标对象自身的可遍历属性。
+
+（6）getOwnPropertyDescriptor(target, propKey)
+
+拦截Object.getOwnPropertyDescriptor(proxy, propKey)，返回属性的描述对象。
+
+（7）defineProperty(target, propKey, propDesc)
+
+拦截Object.defineProperty(proxy, propKey, propDesc）、Object.defineProperties(proxy, propDescs)，返回一个布尔值。
+
+（8）preventExtensions(target)
+
+拦截Object.preventExtensions(proxy)，返回一个布尔值。
+
+（9）getPrototypeOf(target)
+
+拦截Object.getPrototypeOf(proxy)，返回一个对象。
+
+（10）isExtensible(target)
+
+拦截Object.isExtensible(proxy)，返回一个布尔值。
+
+（11）setPrototypeOf(target, proto)
+
+拦截Object.setPrototypeOf(proxy, proto)，返回一个布尔值。
+
+如果目标对象是函数，那么还有两种额外操作可以拦截。
+
+（12）apply(target, object, args)
+
+拦截 Proxy 实例作为函数调用的操作，比如proxy(...args)、proxy.call(object, ...args)、proxy.apply(...)。
+
+（13）construct(target, args)
+
+拦截 Proxy 实例作为构造函数调用的操作，比如new proxy(...args)。
+ * 
+ */
