c4ec8d17226bc9591a68ffb8c0f32f4121a6ec97
潘林峰 <631912579@qq.com>
2020-11-01 09:40:07 +0800

    b

--- 14-Generator函数/03-next方法参数.js
+++ 14-Generator函数/03-next方法参数.js
@@ -1,7 +1,7 @@
 /**
  yield句本身没有返回值，或者说总是返回undefined。next方法可以带一个参数，该参数就会被当作上一个yield语句的返回值。
  */
-if (1) {
+if (0) {
   function* f() {
     for (let i = 0; true; i++) {
       let reset = yield i
@@ -25,3 +25,67 @@ if (1) {
  通过next方法的参数，就有办法在 Generator 函数开始运行之后，继续向函数体内部注入值。
  也就是说，可以在 Generator 函数运行的不同阶段，从外部向内部注入不同的值，从而调整函数行为。
  */
+//再看一个例子。
+if (0) {
+  function* foo(x) {
+    var y = 2 * (yield x + 1)
+    var z = yield y / 3
+    return x + y + z
+  }
+  const a = foo(5)
+  console.log(a.next()) //6
+  console.log(a.next())
+  console.log(a.next())
+
+  const b = foo(5)
+  console.log(b.next())
+  console.log(b.next(12))
+  console.log(b.next(16))
+}
+/**
+ * 上面代码中，第二次运行next方法的时候不带参数，导致y的值等于2 * undefined（即NaN），除以3以后还是NaN，
+ * 因此返回对象的value属性也等于NaN。第三次运行Next方法的时候不带参数，所以z等于undefined，返回对象的value属性等于5 + NaN + undefined，即NaN。
+ * 
+ * 如果向next方法提供参数，返回结果就完全不一样了。上面代码第一次调用b的next方法时，返回x+1的值6；
+ * 第二次调用next方法，将上一次yield语句的值设为12，因此y等于24，返回y / 3的值8；
+ * 第三次调用next方法，将上一次yield语句的值设为13，因此z等于13，这时x等于5，y等于24，所以return语句的值等于42。
+ * 
+ * 
+注意，由于next方法的参数表示上一个yield语句的返回值，所以第一次使用next方法时，不能带有参数。
+V8引擎直接忽略第一次使用next方法时的参数，
+只有从第二次使用next方法开始，参数才是有效的。从语义上讲，第一个next方法用来启动遍历器对象，所以不用带有参数。
+
+如果想要第一次调用next方法时，就能够输入值，可以在Generator函数外面再包一层。
+ */
+if (0) {
+  function wrapper(generatorFunction) {
+    return function (...args) {
+      let generatorObject = generatorFunction(...args)
+      generatorObject.next()
+      return generatorObject
+    }
+  }
+  const wrapped = wrapper(function* () {
+    console.log(`First input: ${yield}`)
+    return "DONE"
+  })
+  wrapped().next("hello!")
+}
+//上面代码中，Generator函数如果不用wrapper先包一层，是无法第一次调用next方法，就输入参数的。
+
+//再看一个通过next方法的参数，向Generator函数内部输入值的例子。
+if (1) {
+  function* dataConsumer() {
+    console.log("start")
+    console.log(`1. ${yield}`)
+    console.log(`2. ${yield}`)
+    return "result"
+  }
+  let genObj = dataConsumer()
+  genObj.next()
+  // Started
+  genObj.next("a")
+  // 1. a
+  genObj.next("b")
+  // 2. b
+}
